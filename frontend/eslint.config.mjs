7467c59513db133b79382174cb615a4561f80809
jeanetteobr <jeanette.obr@gmail.com>
2024-12-10 23:18:35 -0500

    chore: Create github actions. Remove unneeded files from frontend project.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,12 +3,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: sanitizeGlobals(globals.browser) } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
+
+function sanitizeGlobals(globals) {
+  return Object.fromEntries(
+    Object.entries(globals).map(([key, value]) => [key.trim(), value])
+  );
+}
